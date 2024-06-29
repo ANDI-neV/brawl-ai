5edad5b1445c9dbddcf20cea2214f89d3af31f37
Leon Bartle <leon@bartle.eu>
2024-06-29 23:30:51 +0200

    Geetting DATA!

--- src/ai/datasource.py
+++ src/ai/datasource.py
@@ -1,6 +1,6 @@
 import requests
 
-class BrawlStarsAPI:
+class BrawlAPI:
     def __init__(self):
         self.url = "https://api.brawlify.com/v1/events"
 
@@ -10,4 +10,23 @@ class BrawlStarsAPI:
             data = response.json()
             return data
         else:
+            return None
+        
+class DevBrawlAPI:
+    # This uses a token
+    def __init__(self):
+        self.url = "https://api.brawlstars.com"
+        self.token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjJiNjY5OTlmLWFkNTUtNDJlZC1iYTMzLTRiM2QzNjdkNmQxOCIsImlhdCI6MTcxOTY5NTQ1Nywic3ViIjoiZGV2ZWxvcGVyL2YxYzg1ODczLTE5YWUtZTkwZi02N2QyLTU0ZWFlYmM3YmFmMSIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiMi4yMDYuMjAyLjE3OCIsIjc3LjQ3LjYyLjIyNCJdLCJ0eXBlIjoiY2xpZW50In1dfQ.A-wYJ6Ok8pMCShviykxGZFEUOzhthLt7pVAh-mgmnQdg5iJYI1OAm-2EjYSJ9QYE1JBfP5a3N3PtNID97yXPtg"
+    def getPlayerStats(self, playerTag):
+        headers = {
+            "Authorization": f"Bearer {self.token}"
+        }
+        print("Requesting from" + f"{self.url}/v1/players/%23{playerTag}/battlelog")
+        response = requests.get(f"{self.url}/v1/players/%23{playerTag}/battlelog", headers=headers)
+        if response.status_code == 200:
+            data = response.json()
+            return data
+        else:
+            # parse the ClientError Model
+            print(response.status_code)
             return None
--- src/main.py
+++ src/main.py
@@ -1,6 +1,11 @@
-from ai.datasource import BrawlStarsAPI
+from ai.datasource import DevBrawlAPI
+import json
+
+# Leon Tag: 2QYUPPUG8
 
 if __name__ == "__main__":
-    api = BrawlStarsAPI()
-    print(api.get_data())
+    api = DevBrawlAPI()
+    player = api.getPlayerStats("2QYUPPUG8")
+    parsed = json.dumps(player, indent=4)  
+    print(parsed)
 
