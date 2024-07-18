b9b901aa8c0814095998ec3fafd1df4a0d06fb10
Leon Bartle <leon@bartle.eu>
2024-07-18 21:24:52 +0200

    Update datasource.py

--- src/ai/datasource.py
+++ src/ai/datasource.py
@@ -1,16 +1,5 @@
 import requests
 
-class BrawlAPI:
-    def __init__(self):
-        self.url = "https://api.brawlify.com/v1/events"
-
-    def get_data(self):
-        response = requests.get(self.url)
-        if response.status_code == 200:
-            data = response.json()
-            return data
-        else:
-            return None
         
 class DevBrawlAPI:
     # This uses a token
