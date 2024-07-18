4c7e676c8def7442ba33f62b4a0110deb14659ad
Leon Bartle <leon@bartle.eu>
2024-07-19 00:05:53 +0200

    Database scraping

--- src/ai/datasource.py
+++ src/ai/datasource.py
@@ -5,13 +5,26 @@ class DevBrawlAPI:
     # This uses a token
     def __init__(self):
         self.url = "https://api.brawlstars.com"
-        self.token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjJiNjY5OTlmLWFkNTUtNDJlZC1iYTMzLTRiM2QzNjdkNmQxOCIsImlhdCI6MTcxOTY5NTQ1Nywic3ViIjoiZGV2ZWxvcGVyL2YxYzg1ODczLTE5YWUtZTkwZi02N2QyLTU0ZWFlYmM3YmFmMSIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiMi4yMDYuMjAyLjE3OCIsIjc3LjQ3LjYyLjIyNCJdLCJ0eXBlIjoiY2xpZW50In1dfQ.A-wYJ6Ok8pMCShviykxGZFEUOzhthLt7pVAh-mgmnQdg5iJYI1OAm-2EjYSJ9QYE1JBfP5a3N3PtNID97yXPtg"
+        self.token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjhkZDViMmIyLTA1ZDItNDJmNC04MDdkLTg3MzgyNWI0NmYyYSIsImlhdCI6MTcyMTMzMTU0OSwic3ViIjoiZGV2ZWxvcGVyL2YxYzg1ODczLTE5YWUtZTkwZi02N2QyLTU0ZWFlYmM3YmFmMSIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiMi4yMDMuMTk3LjIyNyJdLCJ0eXBlIjoiY2xpZW50In1dfQ.H8wmDltmi0vfaqrDphmvhsDC1y_XKSR23Gv-yN_oO8G5uXrORRG2-Rz8YW3b8dkUUrvHK5efr6nZwrauJxMkBg"
+    def getPlayerBattlelog(self, playerTag):
+        headers = {
+            "Authorization": f"Bearer {self.token}"
+        }
+        print("Requesting from " + f"{self.url}/v1/players/%23{playerTag}/battlelog")
+        response = requests.get(f"{self.url}/v1/players/%23{playerTag}/battlelog", headers=headers)
+        if response.status_code == 200:
+            data = response.json()
+            return data
+        else:
+            # parse the ClientError Model
+            print(response.status_code)
+            return None
+        
     def getPlayerStats(self, playerTag):
         headers = {
             "Authorization": f"Bearer {self.token}"
         }
-        print("Requesting from" + f"{self.url}/v1/players/%23{playerTag}/battlelog")
-        response = requests.get(f"{self.url}/v1/players/%23{playerTag}/battlelog", headers=headers)
+        response = requests.get(f"{self.url}/v1/players/%23{playerTag}", headers=headers)
         if response.status_code == 200:
             data = response.json()
             return data
---
+++ src/db.py
@@ -0,0 +1,72 @@
+import sqlite3
+import random
+
+class Database:
+    def __init__(self):
+        self.conn = sqlite3.connect("games.db")
+        self.cur = self.conn.cursor()
+        self.cur.execute("CREATE TABLE IF NOT EXISTS battles (id INTEGER PRIMARY KEY, battleTime INTEGER, map TEXT, mode TEXT, a1 TEXT, a2 TEXT, a3 TEXT, b1 TEXT, b2 TEXT, b3 TEXT, result INTEGER)")
+        self.cur.execute("CREATE TABLE IF NOT EXISTS players (tag TEXT PRIMARY KEY, name TEXT, checked INTEGER)")
+        self.conn.commit()
+
+    def get_unchecked_player(self):
+        self.cur.execute("SELECT * FROM players WHERE checked=0 LIMIT 1")
+        return self.cur.fetchone()
+    
+    def get_battles_count(self):
+        self.cur.execute("SELECT COUNT(*) FROM battles")
+        return self.cur.fetchone()[0]
+    
+    def get_players_count(self):
+        self.cur.execute("SELECT COUNT(*) FROM players")
+        return self.cur.fetchone()[0]
+    
+    def insert_battle(self, battle):
+        # check by BattleTime
+        if self.cur.execute("SELECT * FROM battles WHERE battleTime=?", (battle[1],)).fetchone():
+            return
+        self.cur.execute("INSERT INTO battles (id, battleTime, map, mode, a1, a2, a3, b1, b2, b3, result) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", battle)
+        self.conn.commit()
+
+    def insert_player(self, player):
+        tag = player[0]
+        name = player[1]
+        if self.cur.execute("SELECT * FROM players WHERE tag=?", (tag,)).fetchone():
+            return
+        self.cur.execute("INSERT INTO players (tag, name, checked) VALUES (?, ?, ?)", (tag, name, 0))
+        self.conn.commit()
+
+    def set_player_checked(self, tag):
+        self.cur.execute("UPDATE players SET checked=1 WHERE tag=?", (tag,))
+        self.conn.commit()
+
+    def getWinrate(self, brawler):
+        self.cur.execute("SELECT COUNT(*) FROM battles WHERE (a1=? OR a2=? OR a3=?) AND result=1", (brawler, brawler, brawler))
+        wins = self.cur.fetchone()[0]
+        self.cur.execute("SELECT COUNT(*) FROM battles WHERE (b1=? OR b2=? OR b3=?) AND result=0", (brawler, brawler, brawler))
+        wins += self.cur.fetchone()[0]
+        self.cur.execute("SELECT COUNT(*) FROM battles WHERE (a1=? OR a2=? OR a3=?)", (brawler, brawler, brawler))
+        total = self.cur.fetchone()[0]
+        self.cur.execute("SELECT COUNT(*) FROM battles WHERE (b1=? OR b2=? OR b3=?)", (brawler, brawler, brawler))
+        total += self.cur.fetchone()[0]
+        return wins / total
+    
+    def getAllBrawlers(self):
+        self.cur.execute("SELECT DISTINCT a1 FROM battles")
+        a1 = self.cur.fetchall()
+        self.cur.execute("SELECT DISTINCT a2 FROM battles")
+        a2 = self.cur.fetchall()
+        self.cur.execute("SELECT DISTINCT a3 FROM battles")
+        a3 = self.cur.fetchall()
+        self.cur.execute("SELECT DISTINCT b1 FROM battles")
+        b1 = self.cur.fetchall()
+        self.cur.execute("SELECT DISTINCT b2 FROM battles")
+        b2 = self.cur.fetchall()
+        self.cur.execute("SELECT DISTINCT b3 FROM battles")
+        b3 = self.cur.fetchall()
+        brawlers = []
+        for brawler in a1 + a2 + a3 + b1 + b2 + b3:
+            brawlers.append(brawler[0])
+        # remove duplicates
+        brawlers = list(dict.fromkeys(brawlers))
+        return brawlers
