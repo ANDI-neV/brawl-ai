dafaa3dd98de3e360ddff34504ddd39da8e54547
Oleg Tolochko <oleg.i.tolochko@gmail.com>
2024-07-21 01:03:58 +0200

    Add scraper and setup ai.py

--- src/ai.py
+++ src/ai.py
@@ -1,2 +1,10 @@
 # we do stuff here
-import schwanz
+import torch
+import pandas as pd
+import numpy as np
+import requests
+from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
+
+brawler_data = [
+
+]
