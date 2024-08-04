aaeb0230e3145dafb5ece49cf002d30ec1321499
Oleg Tolochko <oleg.i.tolochko@gmail.com>
2024-08-04 22:27:01 +0200

    Clean up code and significantly improve AI performance

--- src/ai.py
+++ src/ai.py
@@ -1,23 +1,19 @@
-# we do stuff here
-import pandas as pd
-import os
 import json
-import numpy as np
 from sqlalchemy import create_engine
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from sklearn.datasets import make_classification
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import DataLoader, TensorDataset, random_split
+from torch.utils.data import TensorDataset, DataLoader, random_split
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.model_selection import train_test_split
+import numpy as np
+import pandas as pd
 from joblib import dump
 
 def prepare_training_data():
     engine = create_engine('sqlite:///../games.db')
 
-    match_data = pd.read_sql_query("SELECT * FROM battles WHERE map='Out in the Open'", con=engine)
-
+    match_data = pd.read_sql_query("SELECT * FROM battles WHERE map='Undermine'", con=engine)
     match_data = match_data.drop(["id", "battleTime", "mode", "map"], axis=1)
 
     return match_data
@@ -27,224 +23,177 @@ def prepare_brawler_data():
         brawler_data = json.load(json_file)
 
     return brawler_data
-def get_brawler_features(brawler_name, brawler_data, encoder, index_weight=10):
-    brawler = brawler_data[brawler_name]
-    index_encoded = encoder.transform([[brawler['index']]]).toarray()[0] * index_weight
-    continuous_features = [
-        brawler['movement speed']['normal'],
-        brawler['range']['normal'],
-        brawler['level stats']['health']['11']
-    ]
-    return index_encoded, continuous_features
 
+class BrawlStarsNN(nn.Module):
+    def __init__(self, input_size):
+        super(BrawlStarsNN, self).__init__()
+        self.fc1 = nn.Linear(input_size, 256)
+        self.bn1 = nn.BatchNorm1d(256)
+        self.fc2 = nn.Linear(256, 128)
+        self.bn2 = nn.BatchNorm1d(128)
+        self.fc3 = nn.Linear(128, 64)
+        self.bn3 = nn.BatchNorm1d(64)
+        self.fc4 = nn.Linear(64, 1)
+        self.dropout = nn.Dropout(0.3)
 
-def create_brawler_matrix(match_data, brawler_data, scaler, limit=None, index_weight=10):
+    def forward(self, x):
+        x = torch.relu(self.bn1(self.fc1(x)))
+        x = self.dropout(x)
+        x = torch.relu(self.bn2(self.fc2(x)))
+        x = self.dropout(x)
+        x = torch.relu(self.bn3(self.fc3(x)))
+        x = self.dropout(x)
+        x = torch.sigmoid(self.fc4(x))
+        return x
+
+def create_brawler_matrix(match_data, brawler_data, scaler, encoder, limit=None):
     vectors = []
     results = []
-    unique_indices = np.array([[brawler['index']] for brawler in brawler_data.values()])
-    encoder = OneHotEncoder()
-    encoder.fit(unique_indices)
 
     if limit is not None:
         match_data = match_data.head(limit)
 
     for index, row in match_data.iterrows():
         match_vector = []
-        match_vector_reversed = []
         for team in ['a', 'b']:
-            team_vectors = []
+            team_vector = []
             for i in range(1, 4):
                 brawler_col = f'{team}{i}'
                 brawler_name = str.lower(row[brawler_col])
-                index_encoded, continuous_features = get_brawler_features(brawler_name, brawler_data, encoder, index_weight)
+                brawler = brawler_data[brawler_name]
+
+                # One-hot encode brawler index
+                index_encoded = encoder.transform([[brawler['index']]]).toarray()[0]
+
+                # Get continuous features
+                continuous_features = [
+                    brawler['movement speed']['normal'],
+                    brawler['range']['normal'],
+                    brawler['level stats']['health']['11']
+                ]
                 continuous_features_scaled = scaler.transform([continuous_features])[0]
-                vector = np.concatenate((index_encoded, continuous_features_scaled))
-                team_vectors.append(vector)
 
-            team_vectors.sort(key=lambda x: np.argmax(x[:encoder.categories_[0].shape[0]]))
-            for vector in team_vectors:
-                match_vector.extend(vector)
+                # Combine features
+                brawler_vector = np.concatenate((index_encoded, continuous_features_scaled))
+                team_vector.extend(brawler_vector)
 
-            for vector in reversed(team_vectors):
-                match_vector_reversed.extend(vector)
+            match_vector.extend(team_vector)
 
         vectors.append(match_vector)
         results.append(row['result'])
-        vectors.append(match_vector_reversed)
-        results.append(1 - row['result'])
+
+    return np.array(vectors), np.array(results)
 
 
-    vectors = np.array(vectors)
-    results = np.array(results)
-    return vectors, results
+def train_model(X, y, epochs=80, batch_size=64, learning_rate=0.001):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+    X_train_tensor = torch.FloatTensor(X_train)
+    y_train_tensor = torch.FloatTensor(y_train).unsqueeze(1)
+    X_test_tensor = torch.FloatTensor(X_test)
+    y_test_tensor = torch.FloatTensor(y_test).unsqueeze(1)
 
+    train_dataset = TensorDataset(X_train_tensor, y_train_tensor)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 
-# Define the neural network model
-class NeuralNetwork(nn.Module):
-    def __init__(self, input_size):
-        super(NeuralNetwork, self).__init__()
-        self.fc0 = nn.Linear(input_size, 1024)
-        self.bn0 = nn.BatchNorm1d(1024)
-        self.dropout0 = nn.Dropout(0.5)
-        self.fc1 = nn.Linear(1024, 512)
-        self.bn1 = nn.BatchNorm1d(512)
-        self.dropout1 = nn.Dropout(0.5)
-        self.fc2 = nn.Linear(512, 256)
-        self.bn2 = nn.BatchNorm1d(256)
-        self.dropout2 = nn.Dropout(0.5)
-        self.fc3 = nn.Linear(256, 128)
-        self.bn3 = nn.BatchNorm1d(128)
-        self.dropout3 = nn.Dropout(0.5)
-        self.fc4 = nn.Linear(128, 64)
-        self.bn4 = nn.BatchNorm1d(64)
-        self.dropout4 = nn.Dropout(0.5)
-        self.fc5 = nn.Linear(64, 1)
-        self.sigmoid = nn.Sigmoid()
+    model = BrawlStarsNN(X.shape[1])
+    criterion = nn.BCELoss()
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=5, factor=0.5)
 
-    def forward(self, x):
-        x = self.dropout0(self.bn0(torch.relu(self.fc0(x))))
-        x = self.dropout1(self.bn1(torch.relu(self.fc1(x))))
-        x = self.dropout2(self.bn2(torch.relu(self.fc2(x))))
-        x = self.dropout3(self.bn3(torch.relu(self.fc3(x))))
-        x = self.dropout4(self.bn4(torch.relu(self.fc4(x))))
-        x = self.sigmoid(self.fc5(x))
-        return x
+    for epoch in range(epochs):
+        model.train()
+        for batch_X, batch_y in train_loader:
+            optimizer.zero_grad()
+            outputs = model(batch_X)
+            loss = criterion(outputs, batch_y)
+            loss.backward()
+            optimizer.step()
 
-class ConfidenceLoss(nn.Module):
-    def __init__(self):
-        super(ConfidenceLoss, self).__init__()
-        self.bce_loss = nn.BCELoss()
+        model.eval()
+        with torch.no_grad():
+            train_outputs = model(X_train_tensor)
+            train_loss = criterion(train_outputs, y_train_tensor)
+            train_accuracy = ((train_outputs > 0.5) == y_train_tensor).float().mean()
 
-    def forward(self, outputs, targets):
-        bce_loss = self.bce_loss(outputs, targets)
-        confidence_penalty = torch.mean(torch.abs(0.5 - outputs))
-        return bce_loss - confidence_penalty
+            test_outputs = model(X_test_tensor)
+            test_loss = criterion(test_outputs, y_test_tensor)
+            test_accuracy = ((test_outputs > 0.5) == y_test_tensor).float().mean()
 
-def train(model, loader, criterion, optimizer, device):
-    model.train()
-    running_loss = 0.0
-    for inputs, targets in loader:
-        inputs, targets = inputs.to(device), targets.to(device)
+        scheduler.step(test_loss)
 
-        optimizer.zero_grad()
-        outputs = model(inputs)
-        loss = criterion(outputs, targets)
-        loss.backward()
-        optimizer.step()
-        running_loss += loss.item() * inputs.size(0)
-    return running_loss / len(loader.dataset)
+        if (epoch + 1) % 5 == 0:
+            print(f"Epoch {epoch + 1}/{epochs}")
+            print(f"Train Loss: {train_loss:.4f}, Train Accuracy: {train_accuracy:.4f}")
+            print(f"Test Loss: {test_loss:.4f}, Test Accuracy: {test_accuracy:.4f}")
+            print("---")
 
-def evaluate(model, loader, criterion, device):
-    model.eval()
-    running_loss = 0.0
-    correct = 0
-    with torch.no_grad():
-        for inputs, targets in loader:
-            inputs, targets = inputs.to(device), targets.to(device)
-            outputs = model(inputs)
-            loss = criterion(outputs, targets)
-            running_loss += loss.item() * inputs.size(0)
-            predictions = (outputs > 0.5).float()
-            correct += (predictions == targets).sum().item()
-    return running_loss / len(loader.dataset), correct / len(loader.dataset)
+    return model
 
-def train_model():
+if __name__ == '__main__':
     match_data = prepare_training_data()
     brawler_data = prepare_brawler_data()
 
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    print("Num GPUs Available: ", torch.cuda.device_count())
-
-    continuous_features = []
-    for brawler_name, brawler in brawler_data.items():
-        continuous_features.append([
-            brawler['movement speed']['normal'],
-            brawler['range']['normal'],
-            brawler['level stats']['health']['11']
-        ])
-
-    scaler = StandardScaler()
-    scaler.fit(continuous_features)
-
-    X, y = create_brawler_matrix(match_data, brawler_data, scaler)
-
-    dump(scaler, 'std_scaler.bin', compress=True)
-
-    X_tensor = torch.tensor(X, dtype=torch.float32)
-    y_tensor = torch.tensor(y, dtype=torch.float32).unsqueeze(1)
-
-    dataset = TensorDataset(X_tensor, y_tensor)
-    train_size = int(0.8 * len(dataset))
-    test_size = len(dataset) - train_size
-    train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
-    train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-    test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
-
-    print(X.shape[1])
-    print(X)
-    model = NeuralNetwork(X.shape[1]).to(device)
-    #criterion = nn.BCELoss()
-    criterion = ConfidenceLoss()
-
-    optimizer = optim.Adam(model.parameters(), lr=0.0001)
-    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.2, patience=5, min_lr=0.00001)
-
-    best_val_loss = float('inf')
-    patience = 30
-    patience_counter = 0
-
-    for epoch in range(100):
-        train_loss = train(model, train_loader, criterion, optimizer, device)
-        val_loss, val_accuracy = evaluate(model, test_loader, criterion, device)
-
-        print(f"Epoch {epoch+1}, Train Loss: {train_loss:.4f}, Val Loss: {val_loss:.4f}, Val Accuracy: {val_accuracy:.4f}")
-
-        scheduler.step(val_loss)
-
-        if val_loss < best_val_loss:
-            best_val_loss = val_loss
-            torch.save(model.state_dict(), 'best_model.pth')
-            patience_counter = 0
-        else:
-            patience_counter += 1
-
-        if patience_counter >= patience:
-            print("Early stopping triggered")
-            break
-
-    model.load_state_dict(torch.load('best_model.pth'))
-    test_loss, test_accuracy = evaluate(model, test_loader, criterion, device)
-    print(f'Test Loss: {test_loss:.4f}, Test Accuracy: {test_accuracy:.4f}')
-
-def prepare_input_data(combination, brawler_data, index_weight=10):
+    # Prepare encoders and scalers
     unique_indices = np.array([[brawler['index']] for brawler in brawler_data.values()])
     encoder = OneHotEncoder()
     encoder.fit(unique_indices)
 
+    continuous_features = [
+        [brawler['movement speed']['normal'], brawler['range']['normal'], brawler['level stats']['health']['11']]
+        for brawler in brawler_data.values()
+    ]
+    scaler = StandardScaler()
+    scaler.fit(continuous_features)
+
+    dump(encoder, 'encoder.joblib')
+    dump(scaler, 'scaler.joblib')
+
+    # Create feature matrix
+    X, y = create_brawler_matrix(match_data, brawler_data, scaler, encoder)
+
+    # Train the model
+    model = train_model(X, y)
+
+    # Save the model
+    torch.save(model.state_dict(), 'brawl_stars_model_undermine.pth')
+
+
+def get_brawler_features(brawler_name, brawler_data):
+    brawler = brawler_data[brawler_name]
+    continuous_features = [
+        brawler['movement speed']['normal'],
+        brawler['range']['normal'],
+        brawler['level stats']['health']['11']
+    ]
+    return brawler['index'], continuous_features
+
+
+def prepare_input_data(combination, brawler_data, encoder, scaler):
     match_vector = []
     for team in ['a', 'b']:
-        team_vectors = []
         for i in range(1, 4):
             brawler_col = f'{team}{i}'
             brawler_name = str.lower(combination[brawler_col])
-            features, continuous_features = get_brawler_features(brawler_name, brawler_data, encoder, index_weight)
-            team_vectors.append(features)
+            brawler_index, continuous_features = get_brawler_features(brawler_name, brawler_data)
 
-        team_vectors.sort(key=lambda x: np.argmax(x[:encoder.categories_[0].shape[0]]))
-        for vector in team_vectors:
-            match_vector.extend(vector)
+            # One-hot encode brawler index
+            index_encoded = encoder.transform([[brawler_index]]).toarray()[0]
 
-    match_vector = np.array(match_vector)
-    return match_vector.reshape(1, -1)
+            # Scale continuous features
+            continuous_features_scaled = scaler.transform([continuous_features])[0]
 
-def normalize_input_data(input_data, scaler):
-    normalized_data = scaler.transform(input_data)
-    return normalized_data
+            # Combine features
+            brawler_vector = np.concatenate((index_encoded, continuous_features_scaled))
+            match_vector.extend(brawler_vector)
+
+    return np.array(match_vector).reshape(1, -1)
+
 
 def predict_win_probability(model, input_data, device):
+    model.eval()
     with torch.no_grad():
-        input_tensor = torch.tensor(input_data, dtype=torch.float32).to(device)
+        input_tensor = torch.FloatTensor(input_data).to(device)
         output = model(input_tensor)
-        probability = output.cpu().numpy()[0][0]
+        probability = output.item()
     return probability
--- src/test.py
+++ src/test.py
@@ -3,29 +3,28 @@ from src.ai import *
 from torchsummary import summary
 from joblib import load
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+encoder = load('encoder.joblib')
+scaler = load('scaler.joblib')
+
+# Example usage
 brawler_data = prepare_brawler_data()
 
 combination = {
-    'a1': 'angelo',
-    'a2': 'piper',
-    'a3': 'brock',
-    'b1': 'shelly',
-    'b2': 'poco',
-    'b3': 'sprout'
+    'a1': 'griff',
+    'a2': 'rico',
+    'a3': 'chester',
+    'b1': 'surge',
+    'b2': 'pam',
+    'b3': 'penny'
 }
 
-input_data = prepare_input_data(combination, brawler_data)
+input_data = prepare_input_data(combination, brawler_data, encoder, scaler)
 
-#scaler = load('std_scaler.bin')
-normalized_data = input_data
-#normalized_data = normalize_input_data(input_data, scaler)
-print(normalized_data)
-
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = NeuralNetwork(normalized_data.shape[1]).to(device)
-
-model.load_state_dict(torch.load('best_model.pth'))
+model = BrawlStarsNN(input_data.shape[1]).to(device)
+model.load_state_dict(torch.load('brawl_stars_model_undermine.pth'))
 model.eval()
 
-win_probability = predict_win_probability(model, normalized_data, device)
+win_probability = predict_win_probability(model, input_data, device)
 print(f'Win Probability: {win_probability:.4f}')
