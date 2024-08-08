88fd9ffef15e46d61e80a98aee8f0bcc20c0308d
Leon Bartle <leon@bartle.eu>
2024-08-08 19:30:52 +0200

    fix db, forgot cur

--- src/db.py
+++ src/db.py
@@ -23,6 +23,10 @@ class Database:
             )
         except (psycopg2.DatabaseError, Exception) as error:
             print(error)
+        self.cur = self.conn.cursor()
+        self.cur.execute("CREATE TABLE IF NOT EXISTS battles (id INTEGER PRIMARY KEY, battleTime INTEGER, map TEXT, mode TEXT, a1 TEXT, a2 TEXT, a3 TEXT, b1 TEXT, b2 TEXT, b3 TEXT, result INTEGER)")
+        self.cur.execute("CREATE TABLE IF NOT EXISTS players (tag TEXT PRIMARY KEY, name TEXT, checked INTEGER)")
+        self.conn.commit()
 
     def commit(self):
         self.conn.commit()
